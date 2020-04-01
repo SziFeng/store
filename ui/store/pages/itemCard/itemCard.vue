b492ef68dc36ef6e95ef0adbf9f0af0e1e8567bc
LeongLRF <47407807+LeongLRF@users.noreply.github.com>
2020-04-01 17:53:39 +0800

    1

---
+++ ui/store/pages/itemCard/itemCard.vue
@@ -0,0 +1,36 @@
+<template>
+	<view>
+		<view class="card">	
+			<view class="picture">
+				<image src="/static/image4.jpg" mode=" widthFix"/>
+			</view>
+			<view class="name"></view>
+			<view class="desc"></view>
+			<view class="price"></view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'itemCard',
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.card{
+		border-radius: 5%;
+		width: 110px;
+		height: 120px;
+		border: #EEEEEE solid 1rpx;
+	}
+
+</style>
